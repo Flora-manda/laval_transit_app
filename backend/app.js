3e8960380c0f7afe47c2052a6b665ee5cf5c6f34
Flora Uwadiegwu <flora@Floras-MacBook-Pro.local>
2020-06-06 16:25:58 -0400

    update app.js and installed cors package

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const cors = require("cors");
 const mapsRouter = require("./routes/maps");
 const mapHelpers = require("./helpers/mapHelpers");
 const bodyParser = require("body-parser");
@@ -15,31 +13,24 @@ const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // for accepting json from the client
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/maps", mapsRouter(mapHelpers));
-
+app.use("/mapbbox", mapsRouter(mapHelpers));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render("error");
