49a33374b5cb8ae03ff157e140eb2ccdbdd5187f
Flora Uwadiegwu <flora@Floras-MacBook-Pro.local>
2020-06-06 16:37:00 -0400

    update react component for frontend and used mapbox integrated classes directly

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,47 @@
-import React, { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
+import React from "react";
+import "./App.css";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
 
-function App() {
-  const [viewport, setViewport] = useState({
-    latitude: 45.5757802,
-    longitude: -73.7530656,
-    zoom: 11.5,
-    width: "50vw",
-    height: "50vh",
-  });
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
-  return (
-    <div className="App">
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/fuwadiegwu/ckb2gfamh0t431ip3peld2jn8"
-        onViewportChange={(viewport) => {
-          setViewport(viewport);
-        }}
-      >
-        //markers go here in future
-      </ReactMapGL>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    // create map container
+    const map = new mapboxgl.Map({
+      container: this.mapWrapper,
+      style: "mapbox://styles/mapbox/streets-v10",
+      center: [-73.707092, 45.612499],
+      zoom: 12,
+    });
+    // fetch bbox values from backend and constrain mapbox direction inputs
+    const url = "http://localhost:3002/mapbbox";
+    const response = fetch(url)
+      .then((res) => res.json())
+      .then((bboxData) => {
+        const directions = new MapboxDirections({
+          accessToken: mapboxgl.accessToken,
+          unit: "metric",
+          profile: "mapbox/driving",
+          geocoder: {
+            bbox: bboxData,
+          },
+        });
+
+        map.addControl(directions, "top-left");
+        console.log(bboxData);
+      })
+      .catch((error) => console.log({ error }));
+    // add zoom navigation feature to map
+    map.addControl(new mapboxgl.NavigationControl());
+  }
+
+  render() {
+    return (
+      <div ref={(div) => (this.mapWrapper = div)} className="mapWrapper" />
+    );
+  }
 }
-
 export default App;
