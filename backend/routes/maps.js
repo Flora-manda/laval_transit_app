3cf75c91ccefe5d0e7ffbdeb79e1ee297e569021
Flora Uwadiegwu <flora@Floras-MacBook-Pro.local>
2020-06-06 16:24:03 -0400

    add converted laval area data and update helper function

--- backend/routes/maps.js
+++ backend/routes/maps.js
@@ -3,18 +3,7 @@ var router = express.Router();
 
 module.exports = ({ readMapFile }) => {
   router.get("/", function (req, res, next) {
-    // getting the coordinates from the front-end => bodyParser
-    // checking if it is in the boundary of the Laval map
-
-    console.log("mapHelpers", readMapFile);
-
-    readMapFile().then((bbox) => {
-      const [minLon, minLat, maxLon, maxLat] = bbox;
-
-      // check the boundaries against the provided coordinates
-    });
-
-    res.send("getting bbox");
+    res.send(readMapFile());
   });
 
   return router;
